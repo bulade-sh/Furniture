be47244af5b5f9ad58933026f8693b21b5203dd0
bulade-shaf <bulatshafikov05@gmail.com>
2025-04-27 23:20:04 +0500

    Реализация паттерна проектированния Шаблонный метод

--- main.cpp
+++ main.cpp
@@ -50,107 +50,175 @@ CleaningStrategy* CreateCleaningStrategy(CleaningEnum method) {
     }
 }
 
-class Furniture {
+class Furniture
+{
 private:
     string material;
     FurnitureColor color;
     int weight;
-    CleaningStrategy* cleaningMethod;
+
+    CleaningStrategy* cleaningMethod; // Стратегия очистки
+
+protected:
     bool FurnitureIsExpen;
+
+    // Проверка, можно ли чистить
+    virtual bool CanBeCleaned() { return true; }
+
+    // Подготовка к очистке
+    virtual void PrepareForCleaning() { cout << "Preparing furniture... "; }
+
+    // Очистка
+    void PerformCleaning()
+    {
+        if (cleaningMethod != nullptr)
+        {
+            cleaningMethod->clean();
+        }
+        else
+        {
+            cout << "No cleaning method set!";
+        }
+    }
 
 public:
     Furniture(string mater, FurnitureColor clr, int wgt)
-        : material(mater), color(clr), weight(wgt),
-          cleaningMethod(nullptr), FurnitureIsExpen(false) {
+        : material(mater), color(clr), weight(wgt), cleaningMethod(nullptr)
+    {
         FurnitureIsExpen = static_cast<bool>(rand() % 2);
     }
 
-    virtual ~Furniture() {
+    virtual ~Furniture()
+    {
         if (cleaningMethod != nullptr) delete cleaningMethod;
+    }
+
+    virtual void PrintType() = 0;
+
+    // Шаблонный метод очистки
+    void clean()
+    {
+        if (!CanBeCleaned())
+        {
+            cout << "This furniture cannot be cleaned!" << endl;
+            return;
+        }
+        PrintType();
+        cout << " -> ";
+        PrepareForCleaning();
+        cout << " -> ";
+        PerformCleaning();
+        cout << endl;
+    }
+
+    void SetCleaningStrategy(CleaningStrategy* strategy)
+    {
+        if (cleaningMethod != nullptr) delete cleaningMethod;
+        cleaningMethod = strategy;
     }
 
     virtual void assemble() = 0;
     virtual void disassemble() = 0;
     virtual void punch() { cout << "PUNCH furniture" << endl; }
 
-    void clean() {
-        if (cleaningMethod != nullptr) {
-            cleaningMethod->clean();
-            cout << endl;
-        } else {
-            cout << "No cleaning method set!" << endl;
-        }
-    }
-
-    void SetCleaningStrategy(CleaningStrategy* strategy) {
-        if (cleaningMethod != nullptr) delete cleaningMethod;
-        cleaningMethod = strategy;
-    }
-
     FurnitureColor GetColor() const { return color; }
     bool IsExpen() const { return FurnitureIsExpen; }
 };
 
-class Chair : public Furniture {
+class Chair : public Furniture
+{
 private:
     int numLegs;
     bool hasBack;
 
+protected:
+    void PrepareForCleaning() override
+    {
+        cout << "Checking chair stability... ";
+    }
+
 public:
-    Chair() : Furniture("Wood", FurnitureColor::Brown, 5), numLegs(4), hasBack(true) {
+    Chair() : Furniture("Wood", FurnitureColor::Brown, 5), numLegs(4), hasBack(true)
+    {
+        // Установка стратегии по умолчанию
         SetCleaningStrategy(CreateCleaningStrategy(CleaningEnum::ShakeOff));
     }
 
-    void assemble() override {
+    void assemble() override
+    {
         cout << "Chair assembly with " << numLegs << " legs."
              << (hasBack ? " Has back" : "") << endl;
     }
 
-    void disassemble() override {
+    void disassemble() override
+    {
         cout << "Chair disassembly" << endl;
     }
+
+    void PrintType() { cout << "Chair"; }
 };
 
-class Table : public Furniture {
+class Table : public Furniture
+{
 private:
     string shape;
     int numSeats;
 
+protected:
+    void PrepareForCleaning() override
+    {
+        cout << "Get rid off dishes... ";
+    }
+
 public:
-    Table() : Furniture("Glass", FurnitureColor::Transparent, 11),
-              shape("round"), numSeats(5) {
+    Table() : Furniture("Glass", FurnitureColor::Transparent, 11), shape("round"), numSeats(5)
+    {
         SetCleaningStrategy(CreateCleaningStrategy(CleaningEnum::Wipe));
     }
 
-    void assemble() override {
+    void assemble() override
+    {
         cout << "Table assembly with " << shape << " shape and "
              << numSeats << " seats." << endl;
     }
 
-    void disassemble() override {
+    void disassemble() override
+    {
         cout << "Table disassembly" << endl;
     }
+
+    void PrintType() { cout << "Table"; }
 };
 
-class Sofa : public Furniture {
+class Sofa : public Furniture
+{
 private:
     int numSeats;
     bool canConvertToBed;
 
+protected:
+    void PrepareForCleaning() override
+    {
+        cout << "Remove clothes and pillows... ";
+    }
 public:
-    Sofa() : Furniture("Fabric", FurnitureColor::Beige, 25),
-             numSeats(4), canConvertToBed(true) {
+    Sofa() : Furniture("Fabric", FurnitureColor::Beige, 25), numSeats(4), canConvertToBed(true)
+    {
         SetCleaningStrategy(CreateCleaningStrategy(CleaningEnum::Vacuum));
     }
 
-    void assemble() override {
+    void assemble() override
+    {
         cout << "Sofa assembly with " << numSeats << " seats."
              << (canConvertToBed ? " Can convert to bed." : "") << endl;
     }
 
-    void disassemble() override {
+    void disassemble() override
+    {
         cout << "Sofa disassembly" << endl;
     }
+
+    void PrintType() { cout << "Sofa"; }
 };
 
 
@@ -257,7 +325,7 @@ int main() {
         FurnitureType furniture_type = static_cast<FurnitureType>(furniture_num);
         Furniture *newFurniture = CreateFurniture(furniture_type);
 
-        newFurniture->SetCleaningStrategy(CreateCleaningStrategy(CleaningEnum::Wipe));
+        //newFurniture->SetCleaningStrategy(CreateCleaningStrategy(CleaningEnum::Wipe));
 
         furnitureArray.Add(newFurniture);
     }
